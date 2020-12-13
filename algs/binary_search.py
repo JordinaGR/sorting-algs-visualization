c697fdb34dec850936dd9f579fd2d406115b4f7a
MLX Jordina Gavalda <jordinagavalda@gmail.com>
2020-12-13 12:43:25 +0100

    added binary search

---
+++ algs/binary_search.py
@@ -0,0 +1,42 @@
+import time
+
+def find(data, n, head, tail, drawdata, speed):
+
+    mid = (head + tail) // 2
+
+    drawdata(data, color_func(len(data), head, tail, mid))
+    time.sleep(speed)
+
+    return mid
+
+def binary(data, n, head, tail, drawdata, speed):
+    if head < tail:
+        findinx = find(data, n, head, tail, drawdata, speed)
+        
+        if findinx == n:
+            drawdata(data, ['green' if x == n else ['black'] for x in range(len(data))])
+            time.sleep(speed)
+            return findinx
+
+        elif n < findinx:
+            #left
+            binary(data, n, head, findinx-1, drawdata, speed)
+        else:
+            #right
+            binary(data, n, findinx+1, tail, drawdata, speed)
+
+def color_func(datalen, head, tail, mid):
+    colorarray = []
+    for i in range(datalen):
+        if i >= head and i <= tail:
+            colorarray.append("gray")
+        else:
+            colorarray.append('black')
+
+        if i == mid:
+            colorarray[i] = 'yellow'
+
+        #if i == currindx:
+        #    colorarray[i] = 'green'
+
+    return colorarray
